9e9fd8d2d67a2f45b9842ad1b012f156bae59b37
Dante <101451298+DanteEmery@users.noreply.github.com>
2022-07-24 22:53:19 -0700

    finish lecture 1

--- src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -4,5 +4,18 @@ object ValuesVariablesTypes extends App {
   val x: Int = 42 // TYPES ARE OPTIONAL
   println(x)
   // VALS ARE IMMUTABLE
+  // COMPILER can infer types
 
+  val aString: String = "Hello"; // Semicolons are allow but not necessary for one liners
+  val aBoolean: Boolean = true
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4323
+  val aLong: Long = 43234323213124L // Capital L at end
+  val aFloat: Float = 2.0f // lower case f at end
+  val aDouble: Double = 2.4
+
+  // variables
+  var aVariable: Int = 4 // var are mutable
+  aVariable = 5 // side effects
 }
